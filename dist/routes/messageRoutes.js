4a1b07f4751fffdc3c2b17fcfeb58814be5b535b
Moussa Kouyate <mobytech91@gmail.com>
2025-10-09 12:04:46 +0200

    message

---
+++ dist/routes/messageRoutes.js
@@ -0,0 +1,27 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// src/routes/message.routes.ts
+const express_1 = __importDefault(require("express"));
+const messageController_1 = require("../controllers/messageController");
+const authMiddleware_1 = require("../middleware/authMiddleware");
+const router = express_1.default.Router();
+// Toutes les routes sont protégées
+router.use(authMiddleware_1.authenticateToken);
+// Envoyer un message
+router.post('/', messageController_1.sendMessage);
+// Récupérer une conversation avec un utilisateur spécifique
+router.get('/user/:userId', messageController_1.getConversation);
+// Récupérer une conversation avec un parking spécifique
+router.get('/parking/:parkingId', messageController_1.getParkingConversation);
+// Récupérer toutes les conversations de l'utilisateur connecté
+router.get('/conversations', messageController_1.getUserConversations);
+// Mettre à jour un message
+router.put('/:id', messageController_1.updateMessage);
+// Supprimer un message
+router.delete('/:id', messageController_1.deleteMessage);
+// Marquer les messages comme lus
+router.patch('/read', messageController_1.markMessagesAsRead);
+exports.default = router;
